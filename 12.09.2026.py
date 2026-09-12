1186a2b45a03d5f9f8ce740365e889cb1a07960d
Forstik <gafar.melinikof@mail.ru>
2026-09-12 19:47:46 +0200

    adadadsadadeqad

---
+++ 12.09.2026.py
@@ -0,0 +1,89 @@
+# import random
+# from random import randrange
+# from random import *
+
+# from random import randrange
+# a = randrange(0,50)
+# print(a)
+
+# from random import shuffle
+# numbers = [1,2,3,4,5]
+# shuffle(numbers)
+# print(numbers)
+
+# from random import randint
+# print(randint(1,6))
+
+# from random import *
+# from math import floor
+# a = random()
+# a *= 1000
+# a = a ** 0.5
+# print(floor(a))
+
+# x = 25
+# print(f"{x:b}") # bin
+# print(f"{x:o}") # oct
+# print(f"{x:x}") # hex
+
+# a = 12
+# b = 15
+# print(f"{a+b:x}")
+
+# from random import *
+# print(round(uniform(-10, 10),1))
+
+# from random import sample
+# nums = [10,20,30,40,50,60]
+# print(sample(nums,2))
+
+# from random import *
+# a = randint(1, 20)
+# b = randint(1, 20)
+# print(a > b)
+
+# from math import *
+# from random import uniform
+# a = uniform(10,20)
+# print(ceil(a))
+
+# a =int(input())
+# a = **= 2
+# b = oct(a) #1й
+# print(b[2:])
+# print(format*a,"o")#2й
+
+# x = hex(int((40 - 10) / 3))
+# print(x)
+
+# a = 10
+# b = 15
+# c = 17
+# a,b,c, = 10,15,17
+# num = a * (c - b)
+# num2 = num ** 2
+# num3 = c % 3 + num
+# num4 = num3 // num2
+
+# num = int(input(),16)
+# num2 = int("1010", 2)
+# num3 = num + num2
+# print(bin(num)[2:1], num3)
+
+# x = bin(int((2416-768) / 2))
+# print(x)
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# num1 = (a ** 2)
+# num2 =(b * c)
+# num3 = (num1 + num2)
+# print = (bin(num3)[2:])
+
+# a = int(input(),8)
+# b = int(input(),8)
+# c = a ** 3
+# b = b // 3
+# print(hex(c + b)[2:])
+
